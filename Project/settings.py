3467ede4da4d173a9dd399f487c488c5885f4429
hossainchisty <hossain.chisty11@gmail.com>
2021-05-17 17:39:49 +0600

    add password rest functionality

--- Project/settings.py
+++ Project/settings.py
@@ -50,6 +50,15 @@ INSTALLED_APPS = [
     
 ]
 
+#SMTP Configuration
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'example@gmail.com'
+EMAIL_HOST_PASSWORD = '********'
+
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = "tailwind"
 
--- accounts/urls.py
+++ accounts/urls.py
@@ -3,9 +3,20 @@ from . import views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
+    # Registration and Login
     path('sign-up/', views.register,name="register"),
     path('sign-in/', auth_views.LoginView.as_view(template_name='login.html'),name="login"),
-    path('sign-out/', auth_views.LogoutView.as_view(template_name='logout.html'), name="logout")
+    path('sign-out/', auth_views.LogoutView.as_view(template_name='logout.html'), name="logout"),
+
+
+    # Password Reset with email
+    path('reset_password/', auth_views.PasswordResetView.as_view(template_name='password_reset.html'), name="password_reset"),
+
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name='password_reset_sent.html'), name="password_reset_done"),
+
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='password_reset_form.html'), name="password_reset_confirm"),
+
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_done.html'), name="password_reset_complete"),
 
 ]
 # ratul
