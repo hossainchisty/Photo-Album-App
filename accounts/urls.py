adac11e070aeb3b3e7a4a8ebbd62937786946d78
hossainchisty <hossain.chisty11@gmail.com>
2021-05-17 15:21:16 +0600

    add files

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,30 +1,12 @@
-from typing import AsyncIterable
 from django.urls import path
-from django.contrib.auth import views as auth_views
 from . import views
-
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='registration/reset_password.html'), name='rest_password'),
-
-    path('rest_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name='registration/password_reset_sent.html'), name='password_reset_done'),
-
-    path('rest/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='registration/password_reset_form.html'), name='password_reset_confirm'),
-
-    path('rest_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_done.html'), name='password_reset_complete'),
-
-
-
-    # passworld changes
-    path('change-password/', auth_views.PasswordChangeView.as_view(), name='password_change'),
-    #path('change-password-done/', auth_views.)
-
-
-    # sign-up and sign-in
-    path('sign-up/', views.sign_up, name='sign-up'),
-    
-    path('sign-in/', auth_views.LoginView.as_view(template_name='sign_in.html'), name='sign-in'),
-
-    path('sign-out/', auth_views.LogoutView.as_view(template_name='sign-out.html'), name='sign-out')
+    path('sign-up/', views.register,name="register"),
+    path('sign-in/', auth_views.LoginView.as_view(template_name='login.html'),name="login"),
+    path('sign-out/', auth_views.LogoutView.as_view(template_name='logout.html'), name="logout")
 
 ]
+# ratul
+# /A=wz2pZp#e!"x5m
--- accounts/views.py
+++ accounts/views.py
@@ -1,47 +1,26 @@
-from django.contrib.auth import authenticate
-from django.contrib.auth import login,logout
 from django.shortcuts import render,redirect
-from django.views.decorators.csrf import csrf_exempt
-from django.contrib import messages
-from accounts.forms import UserForm
-# Create your views here.
+from .forms import RegistrationForm
+from django.contrib.auth import authenticate,login
 
-@csrf_exempt
-def sign_up(request):
-    if request.method == 'POST':
-        form = UserForm(request.POST)
+# Create your views here.
+def register(request):
+    if request.method == "POST":
+        form = RegistrationForm(request.POST)
         if form.is_valid():
             form.save()
-            email = request.POST['email']
+            username = request.POST['username']
             password = request.POST['password1']
-            user = authenticate(request, email=email,password=password)
+            
+            user = authenticate(request, username=username,password=password)
             login(request,user)
+            
+            return redirect('/')
 
-            return redirect('sign-in')
     else:
-        form = UserForm()
-
+        form = RegistrationForm()
+    
     context = {'form':form}
 
-    return render(request, 'sign_up.html', context)
+    return render(request, 'register.html', context)
 
 
-
-@csrf_exempt
-def sign_in(request):
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-
-        user = authenticate(request, username=username, password=password)
-        if user is not None:
-        # A backend authenticated the credentials
-                    login(request,user)
-        else:
-        # No backend authenticated the credentials
-                    messages.error(request, 'Credential is incorrect')
-        return redirect('dashboard')
-
-    template_name = 'login.html'
-    context = {}
-    return render(request, template_name, context)
